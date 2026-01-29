7d31eb9acbab3c4922246019b188e424637bb935
zahrsdev <rekabitdev@gmail.com>
2026-01-30 02:24:30 +0700

    feat(02-07): create init stub
    - Display coming soon message for Phase 03
    - Reference external tools installation
    - Provide manual installation instructions
    - List FFmpeg, yt-dlp, MediaPipe requirements

--- src/commands/init.ts
+++ src/commands/init.ts
@@ -5,17 +5,31 @@
  */
 
 import { Command } from 'commander';
-import { log } from '../utils/logger.js';
+import { log, blank, separator } from '../utils/logger.js';
 
 export const initCommand = new Command('init')
   .description('Initialize AutoCliper by installing required dependencies (FFmpeg, yt-dlp)')
   .action(() => {
-    log('Initializing AutoCliper...');
-    log('');
-    log('This will install:');
+    blank();
+    separator();
+    log('AutoCliper Initialization');
+    blank();
+    log('This command will install:');
     log('  - FFmpeg 7.1');
     log('  - yt-dlp 2025.01.x');
     log('  - MediaPipe (optional, for face detection)');
+    blank();
+    log('Coming soon in Phase 03: External Tools Installation');
     log('');
-    log('[Command not yet implemented]');
+    log('For now, please ensure you have the following installed:');
+    blank();
+    log('  1. FFmpeg 7.1 or later');
+    log('     Download from: https://github.com/BtbN/FFmpeg-Builds/releases');
+    blank();
+    log('  2. yt-dlp 2025.01.x or later');
+    log('     Download from: https://github.com/yt-dlp/yt-dlp/releases');
+    blank();
+    log('  3. (Optional) Python with MediaPipe for face detection');
+    log('     Install with: pip install mediapipe opencv-python');
+    separator();
   });
